fdd07ac3b9a0cdf77e16d0d8e9e4f139ce906161
hytome <bjin10241023@gmail.com>
2024-03-20 14:08:19 +0900

    2024_03_19_5702250_자료구조과제2

--- 2024_03_19_5702250_1322.c
+++ 2024_03_19_5702250_1322.c
@@ -2,22 +2,27 @@
 #include <time.h>
 
 
-long power_iter(long a, long b){
+double power_iter(double a, double b){
     //재곱값을 계산하기 위한 반복문 (for)문 방식.
-    long result= 1;  
-    for(int i=0; i<=b; i++){
-        result= result*a;
+    double result= 1; //결과값 저장을 위해 선언.  
+    for(int i=0; i<b; i++){
+        result = result *a;
+        //result 값에 a를 곱하는걸 b까지 반복. 그리고 그합을 계속 result에 저장.
     }
     return result;
+    //그리고 result 반환.
 }
 
-long power_rec(long a, long b){
-    long result_rec = 1;
-    if(b == 0){
-        return a; //왜 이값을 0이나 1로하면 값이 이상해지는거지?
-        //심지어 13의 21같은 큰값이 아니면 또 제데로 제곱값을 방출한다!
+double power_rec(double a, double b){
+    double result_rec = 1;
+    if(b == 0){ //b가 0일경우. 
+        return 1;//더이상 제곱할 값이 없으니 1반환.
     }else{
         return result_rec = a * power_rec(a,b-1);
+        //재귀로 호출 rec(13,21)...rec(13,20)...이렇게 
+        //a*Power_rec(13,21)...(13,20)..
+        //result_rec에 값 넣고 반환.
+
     }
 } 
 
@@ -30,15 +35,15 @@ int main(){
     double time_rec;  //재귀함수로 돌리는 함수의 실행시간을 저장할곳.
 
     start_iter = clock(); //for문으로 돌리는 반복문의 실행시간 측정 시작.
-    long result_iter = power_iter(13,21); //for문으로 돌아가는 함수에 13과 21의 값 삽입
-    printf("for문으로 계산한 13 21의 제곱값은 %ld입니다. \n",result_iter); //그 결과 출력.
+    double result_iter = power_iter(13,21); //for문으로 돌아가는 함수에 13과 21의 값 삽입
+    printf("for문으로 계산한 13 21의 제곱값은 %f입니다. \n",result_iter); //그 결과 출력.
     stop_iter = clock();//for문으로 돌리는 반복문의 실행시간 측정 종료.
     time_iter =(double)(stop_iter-start_iter)/CLOCKS_PER_SEC; //time_iter에 실행시간 측정과 종료를 초로 바꾸어 계산후 저장. 
     printf("측정시간은 %f 입니다.\n",time_iter); //계산한 실행시간을 출력.
 
     start_rec = clock(); //재귀함수 로 돌아가는 함수의 실행시간 측정 시작.
-    long result_rec = power_rec(13,21); //재귀 함수로 돌아가는 함수에 13과 21의 값 삽입.
-    printf("재귀함수로 계산한 13 21의 제곱값은 %ld 입니다. \n",result_rec);
+    double result_rec = power_rec(13,21); //재귀 함수로 돌아가는 함수에 13과 21의 값 삽입.
+    printf("재귀함수로 계산한 13 21의 제곱값은 %f 입니다. \n",result_rec);
     stop_rec = clock(); //재귀함수 로 돌아가는 함수의 실행시간 측정 종료.
     time_rec = (double)(stop_rec-start_rec)/CLOCKS_PER_SEC;
     printf("측정시간은 %f 입니다.",time_rec);
