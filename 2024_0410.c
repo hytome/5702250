21768f6165fad5ecf76cd98b552bc8ddc725a3b8
hytome <bjin10241023@gmail.com>
2024-04-11 10:25:22 +0900

    2024_04_10_자료구조

---
+++ 2024_0410.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define MAX_STACK_SIZE 100 //스택 사이즈  설정.
+//배열을 동적 메모리 할당으로 지정해 스택 프로그램을 짜보자. 
+typedef int element;
+typedef struct {
+    element *data; //data는 포인터로 정의 된다.
+    int capacity; //현재 스택의 크기. 
+    int top;
+}StackType; //stackType 구조체.
+
+//스택 생성 함수. 
+//배열의 시작점 주소를 가져와 계산을 하는게 훨씬 더 좋은 프로그램이기에.
+//포인터를 사용한다 에요.
+void inin_stack(StackType * s){
+    s->top=-1; //스택의 top 값 .초기화 해야하니 -1.
+    s->capacity =  1; //현재 스택의 크기.
+    s->data = (element *) malloc(s->capacity * sizeof(element));
+
+    //s->capacity = nfs
+    //s->data = (element *) malloc(sizeof(StackType)* nfs);
+    //이 경우에는 데이터 10개 설정.
+    //말록 함수로 스택의 사이즈 지정.
+}
+
+//공백 상태 검출 함수
+int is_empty(StackType *s){
+    //공백 상태일 경우 top의 -1;
+    return (s -> top == -1 );
+}
+
+//포화 상태 검출 함수.
+int is_full(StackType *s){
+    //포화 상태일시 스택의 크기를 -1 
+    return (s->top==(s->capacity -1));
+}
+ //이제 푸쉬 함수를 구현해 보자에요. 
+void push(StackType *s, element item){
+    if(is_full(s)){ //포화 상태인 경우.
+        s->capacity *=2; 
+        //공간이 부족한 경우 메모리를 두배로 뻥튀기.
+        s->data = (element *)realloc(s->data,s->capacity * sizeof(element));
+        //relloc로 현재 내용은 유지하면서 주어진 크기로 동적 메모리를 다시 할당한다.
+    } //포화상태가 아닐경우 
+    s->data[++(s->top)]=item;
+}
+//이제 스택에서 삭제를 하는 pop함수를 구현해 보자에요.
+element pop(StackType *s){
+    //element r (pop에서 주석. 교수님이 선호하는 방식.)
+    if(is_empty(s)){
+        //텅 비었을 경우.
+        fprintf(stderr,"스택 공백 에러다에요~\n");
+        exit(1); //프로그램 종료.
+        }else{
+            // r = s->stack[s->top];
+            // s-> top = s->top -1;
+            // return r;
+            return s->data[(s->top)--];
+            //텅 비지 않았을경우 top으로 선입 후출.
+        }
+    }
+int main(){
+    StackType s;
+    //StackType t; //배열 스택 두개로~
+    inin_stack(&s); //주소값으로 설정
+    //init_stack(&t);
+    //srand (time(NULL)); 1.랜덤 번호를 생성하고 짝수면 push 아니면 pop.
+    //int rand _num;
+    //rand_num= rand()% 100 +1;
+    //printf("random num %d \n",rand_num);
+    //if(rand_num<50) //50 보다 작으면 push 아니면 pop.
+    //if(rand_num %2==0){
+        //push(&s,rand_num);
+        //push(&t,rand_num);
+        //printf("push %d\n",rand_num);
+        //printf("push t %d\n",rand_num);
+    //}else{
+        //rand_num=pop(%s);
+        //rand_num=pop(%t);
+        //printf("pop : %d\n",rand_num);
+        //printf("pop t : %d\n",rand_num);
+    //}
+    push(&s,1);
+    push(&s,2);
+    push(&s,3);
+ //차례대로 1~3까지 push
+    printf("%d \n",pop(&s));
+    printf("%d \n",pop(&s));
+    printf("%d \n",pop(&s));
+    free(s.data);
+    //메모리 할당 해제.
+    return 0;
+}
