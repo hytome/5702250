ecc0847b99ff684f839b54a81d950cce1445c0d8
hytome <bjin10241023@gmail.com>
2024-09-20 14:51:36 +0900

    2024-09-20 파이썬 공부

---
+++ python/2024-09-19/파이썬자료형1.py
@@ -0,0 +1,8 @@
+result1= 695 % 20
+print(result1);
+result2= 248 % 100 /5
+print(result2)
+result3= (5 - 7) * 2 ** 2
+print(result3)
+result4= 6 + (18 % (17 - 12))
+print(result4)
---
+++ python/2024-09-19/파이썬자료형2.py
@@ -0,0 +1,9 @@
+# Calculate total owed, assuming 8% tax / 15% tip
+print("Subtotal:")
+SubTotal = 38 + 40 + 30
+Tip = SubTotal * .08
+Tax = SubTotal * .15
+Total = SubTotal + Tip + Tax
+
+print("Subtotal",SubTotal)
+print("Tip",Tip)
