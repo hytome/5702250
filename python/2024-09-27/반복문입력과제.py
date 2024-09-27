a8cd54c8f98974a01abddd3194f615d3ae3b209f
hytome <bjin10241023@gmail.com>
2024-09-27 13:10:17 +0900

    2024-09-27 파이썬 공부

--- python/2024-09-27/반복문입력과제.py
+++ python/2024-09-27/반복문입력과제.py
@@ -1,8 +1,6 @@
-
 def main():
-    age = int(input("How old You: "))
-    years = 65 - age
-    print(years,"Years until retirement!")
-    
-    
-    
+    current_age = int(input("How old are you: "))
+    retirement_age = 65
+    years_until_retirement = retirement_age - current_age
+    print(years_until_retirement, "Years until retirement!")
+
